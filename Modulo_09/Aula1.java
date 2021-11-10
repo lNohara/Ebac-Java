5215bdaf1a3c2cb17fef4fb43c8e66b5ca85d362
= <=>
2021-11-10 12:19:13 -0300

    Módulo 9

---
+++ Modulo_09/Aula1.java
@@ -0,0 +1,21 @@
+package Modulo_09;
+
+public class Aula1 {
+
+    private Integer codigo;
+
+    private Long codigoMaior = 12315464L;
+
+    private Double codigoDouble = 12.5;
+
+    private Boolean status = false;
+
+    private Float codigoFloate = 10.0f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra;
+
+}
