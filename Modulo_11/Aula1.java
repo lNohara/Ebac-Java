e772c26de9e0b381c9b63a468af525910cb76620
= <=>
2021-11-18 12:27:43 -0300

    Mod 11 aula 1

---
+++ Modulo_11/Aula1.java
@@ -0,0 +1,41 @@
+package Modulo_11;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Aula1 {
+
+    /**
+     *  ArrayList é como um array cujotamanho pode crescer
+     *  A busca de um elemento é rápida, mas inserções e exclusões não são
+     */
+
+    private static void exemploListaSimples(){
+        System.out.println("***** exemploListaSimples *****");
+        List<String> lista = new ArrayList<String>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        System.out.println(lista);
+        System.out.println("");
+    }
+
+    private static void exemploListaSimplesOdemAscendente(){
+        System.out.println("**** exemploListaSimplesOdemAscendente ****");
+        List<String> lista = new ArrayList<String>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        Collections.sort(lista);
+        System.out.println(lista);
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        exemploListaSimples();
+        exemploListaSimplesOdemAscendente();
+    }
+
+}
---
+++ Modulo_11/ComparaNotaAluno.java
@@ -0,0 +1,11 @@
+package Modulo_11;
+
+import java.util.Comparator;
+
+public class ComparaNotaAluno implements Comparator<Aluno> {
+
+    @Override
+    public int compare(Aluno o1, Aluno o2) {
+        return Double.compare(o2.getNota(), o1.getNota());
+    }
+}
