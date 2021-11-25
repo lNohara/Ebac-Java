ccfeda1c36435a23bdd89b74d51261cfb0c519c0
= <=>
2021-11-25 11:46:10 -0300

    fim módulo 11

--- Modulo_11/Aluno.java
+++ Modulo_11/Aluno.java
@@ -1,6 +1,8 @@
 package Modulo_11;
 
-public class Aluno implements Comparable<Aluno>{
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno> {
 
     private String nome;
     private String curso;
@@ -67,5 +69,18 @@ public class Aluno implements Comparable<Aluno>{
     @Override
     public int compareTo(Aluno aluno) {
         return this.nome.compareTo(aluno.getNome());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(aluno.nota, nota) == 0 && Objects.equals(nome, aluno.nome) && Objects.equals(curso, aluno.curso) && Objects.equals(sala, aluno.sala);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, curso, nota, sala);
     }
 }
